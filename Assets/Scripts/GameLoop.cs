61ce5ac07e2cf03ca8520f6112324884b075fe02
Charles Wang <charleszw16@gmail.com>
2023-05-11 18:06:15 -0400

    switch to Rider

---
+++ Assets/Scripts/GameLoop.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameLoop : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        
+    }
+}

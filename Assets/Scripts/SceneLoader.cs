e3492d99c24a555d192b74c152f5b65b79613467
Charles Wang <charleszw16@gmail.com>
2023-06-16 17:33:39 -0400

    fix accidental unpause when changing scene

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -5,19 +5,21 @@ using UnityEngine.SceneManagement;
 public class SceneLoader : MonoBehaviour
 {
     public Animator animator;
+    
     private static readonly int Exit = Animator.StringToHash("Exit");
 
     public void LoadSceneWrapper(string sceneName)
     {
         StartCoroutine(LoadScene(sceneName));
-        Time.timeScale = 1;
     }
 
     private IEnumerator LoadScene(string sceneName)
     {
         animator.SetTrigger(Exit);
-        yield return new WaitForSeconds(0.5f);
-
+        yield return new WaitForSecondsRealtime(0.5f);
+        
+        Time.timeScale = 1;
+        
         var asyncLoad = SceneManager.LoadSceneAsync(sceneName);
         while (!asyncLoad.isDone)
         {
